ce8e2d5921cea2b4ce1948eea189832c89981772
thstjddn <sungwoo93@ajou.ac.kr>
2019-01-15 17:20:39 +0900

    xyvaluetest

--- xyvaluetest/deviceorientation.js
+++ xyvaluetest/deviceorientation.js
@@ -1,34 +1,13 @@
 window.addEventListener("deviceorientation", handleOrientation, true);
 
 function handleOrientation(event) {
-    var absolute = event.absolute;
-    var alpha    = event.alpha;
-    var beta     = event.beta;
-    var gamma    = event.gamma;
-  
-    // Do stuff with the new orientation data
-}
-
-var ball   = document.querySelector('.ball');
-var garden = document.querySelector('.garden');
-var output = document.querySelector('.output');
-
-var maxX = garden.clientWidth  - ball.clientWidth;
-var maxY = garden.clientHeight - ball.clientHeight;
-
-function handleOrientation(event) {
-  var x = event.beta;  // In degree in the range [-180,180]
-  var y = event.gamma; // In degree in the range [-90,90]
+  var x = event.beta; 
+  var y = event.gamma; 
 
   output.innerHTML  = "beta : " + x + "\n";
   output.innerHTML += "gamma: " + y + "\n";
 
-  // Because we don't want to have the device upside down
-  // We constrain the x value to the range [-90,90]
   if (x >  90) { x =  90};
   if (x < -90) { x = -90};
-
-
 }
-
 window.addEventListener('deviceorientation', handleOrientation);
