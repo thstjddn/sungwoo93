d22da14efa5e80339a5aac1801ae1e4a3f65b46c
thstjddn <sungwoo93@ajou.ac.kr>
2019-01-15 18:22:50 +0900

    test 14

--- test/js/XYvalue.js
+++ test/js/XYvalue.js
@@ -6,12 +6,9 @@ var output = document.querySelector('.output');
 function handleOrientation(event) {
   var x = -(event.gamma);  // In degree in the range [-180,180]
   var y = event.beta; // In degree in the range [-90,90]
-
-  x.toFixed(1);
-  y.toFixed(1);
   
-  output.innerHTML  = "X : " + x + "\n";
-  output.innerHTML += "Y : " + y + "\n";
+  output.innerHTML  = "X : " + x.toFixed(1) + "\n";
+  output.innerHTML += "Y : " + y.toFixed(1) + "\n";
 
   // Because we don't want to have the device upside down
   // We constrain the x value to the range [-90,90]
