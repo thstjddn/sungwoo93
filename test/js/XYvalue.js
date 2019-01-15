439902630dbb608045c531ea82fdbcab134c6910
thstjddn <sungwoo93@ajou.ac.kr>
2019-01-15 17:47:20 +0900

    xytest

--- test/js/XYvalue.js
+++ test/js/XYvalue.js
@@ -1,13 +1,21 @@
 window.addEventListener("deviceorientation", handleOrientation, true);
 
-function handleOrientation(event) {
-    var x = -event.gamma; 
-    var y = event.beta; 
-  
-    output.innerHTML  = "X : " + x + "\n";
-    output.innerHTML += "Y : " + y + "\n";
 
-    if (x >  90) { x =  90};
-    if (x < -90) { x = -90};
+var output = document.querySelector('.output');
+
+function handleOrientation(event) {
+  var x = -(event.gamma);  // In degree in the range [-180,180]
+  var y = event.beta; // In degree in the range [-90,90]
+
+  
+  output.innerHTML  = "x : " + x + "\n";
+  output.innerHTML += "y : " + y + "\n";
+
+  // Because we don't want to have the device upside down
+  // We constrain the x value to the range [-90,90]
+  if (x >  90) { x =  90};
+  if (x < -90) { x = -90};
+
 }
+
 window.addEventListener('deviceorientation', handleOrientation);
