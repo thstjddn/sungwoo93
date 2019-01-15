0dc5524100ee663ff6c3c7c019394c00d1a20f51
thstjddn <sungwoo93@ajou.ac.kr>
2019-01-15 18:18:48 +0900

    xy test 13

--- test/js/XYvalue.js
+++ test/js/XYvalue.js
@@ -7,6 +7,8 @@ function handleOrientation(event) {
   var x = -(event.gamma);  // In degree in the range [-180,180]
   var y = event.beta; // In degree in the range [-90,90]
 
+  x.toFixed(1);
+  y.toFixed(1);
   
   output.innerHTML  = "X : " + x + "\n";
   output.innerHTML += "Y : " + y + "\n";
