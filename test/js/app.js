eb7dc75da182cbd96f444f129c35848e485022ac
thstjddn <sungwoo93@ajou.ac.kr>
2019-01-17 18:46:35 +0900

    alert windowinnerwidth

--- test/js/app.js
+++ test/js/app.js
@@ -228,11 +228,11 @@
      */
     function setDefaultVariables() {
         screenWidth = window.innerWidth * 0.8;
-        screenHeight = window.innerHeight * 0.8;
+        screenHeight = window.innerHeight * 0.6;
         screenWidth1 = window.innerWidth * 0.8;
         screenHeight1 = window.innerHeight * 0.2;
         screenWidth2 = window.innerWidth * 0.2;
-        screenHeight2 = window.innerHeight * 0.8;
+        screenHeight2 = window.innerHeight * 0.6;
         outerRadius = (screenWidth > screenHeight) ? (screenHeight / 2) : (screenWidth / 2);
         outerRadius1 = outerRadius;
         outerRadius2 = outerRadius;
@@ -265,7 +265,7 @@
             barleft = document.querySelector("#barleft"),
             barleftimg = document.querySelector("#barleft img"),
             errorMessage = document.querySelector("#error-message");
-
+        alert(window.innerwidth*0.2)
         outerCircle.style.left = centerX - outerRadius + "px";
         outerCircle.style.top = centerY - outerRadius + "px";
         outerCircle.style.width = (outerRadius * 2) + "px";
