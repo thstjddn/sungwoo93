eb0cf314dc05929492ec7f0f32f26e1434363f0d
thstjddn <sungwoo93@ajou.ac.kr>
2019-01-21 18:27:33 +0900

    absolute value

--- test/js/XYvalue.js
+++ test/js/XYvalue.js
@@ -8,7 +8,7 @@ var output2 = document.querySelector('.output2');
 function handleOrientation(event) {
     var x = -(event.gamma);  // In degree in the range [-180,180]
     var y = event.beta; // In degree in the range [-90,90]
-    var z = event.alpha;
+    var z = event.absolute;
 
     if(x > 90){
       x = 90;
@@ -33,7 +33,7 @@ function handleOrientation(event) {
     
     output.innerHTML  = "X :" + x.toFixed(1) + "\n";
     output1.innerHTML  = "Y :" + y.toFixed(1) + "\n";
-    // output2.innerHTML = "Z :" + z + "\n";
+    output2.innerHTML = "Z :" + z + "\n";
 
     // Because we don't want to have the device upside down
     // We constrain the x value to the range [-90,90]
