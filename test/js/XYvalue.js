1d26cbc8d65c7ac7ecff7dda351d02e9ffe88cbb
thstjddn <sungwoo93@ajou.ac.kr>
2019-01-21 17:40:04 +0900

    z value test

--- test/js/XYvalue.js
+++ test/js/XYvalue.js
@@ -9,7 +9,7 @@ function handleOrientation(event) {
     var x = -(event.gamma);  // In degree in the range [-180,180]
     var y = event.beta; // In degree in the range [-90,90]
     var z = event.alpha;
-    
+
     if(x > 90){
       x = 90;
       y = 180-y;
@@ -26,6 +26,11 @@ function handleOrientation(event) {
       x = -x;
     };
 
+    if (z < 30 || z>350) {
+      y = -180 - y;
+    }
+
+    
     output.innerHTML  = "X :" + x.toFixed(1) + "\n";
     output1.innerHTML  = "Y :" + y.toFixed(1) + "\n";
     output2.innerHTML = "Z :" + z + "\n";
