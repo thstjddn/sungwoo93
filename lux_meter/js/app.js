54f98db1f1443c26b0421dc30a78f1a58a974113
thstjddn <sungwoo93@ajou.ac.kr>
2019-01-22 16:27:32 +0900

    lux meter 6

--- lux_meter/js/app.js
+++ lux_meter/js/app.js
@@ -1,15 +1,5 @@
 window.addEventListener("devicelight", function (event) {
     // Getting lux
     var luminosity = event.value;
-    alert(luminosity);
-});
-
-window.addEventListener('devicelight', function(event) {
-    var bodyBg= document.body.style;
-    if (event.value < 100) {
-          alert('Hey, you! You are working in the Dim environment');
-          bodyBg.backgroundColor="lightgrey";
-    } else {
-          bodyBg.backgroundColor="#fff";
-    }
+    console.log(luminosity + "lx");
 });
