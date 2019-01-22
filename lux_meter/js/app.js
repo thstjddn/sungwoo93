23b2da6af41f1928ec1d259a9ef1277be11b19e9
thstjddn <sungwoo93@ajou.ac.kr>
2019-01-22 17:33:10 +0900

    dtest

--- lux_meter/js/app.js
+++ lux_meter/js/app.js
@@ -5,12 +5,13 @@ var output = document.querySelector('.output');
 function handleOrientation(event){
  // Getting lux
     var luminosity = event.value;
+    console.log(event)
     output.innerHTML = "LX : " + luminosity + "\n";
 };
 
-function init(){
-    handleOrientation(event);
-}
-window.addEventListener('devicelight', handleOrientation);
+// function init(){
+//     handleOrientation(event);
+// }
+// window.addEventListener('devicelight', handleOrientation);
 
-window.onload = init;
+// window.onload = init;
