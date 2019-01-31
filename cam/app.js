6ce2995c173ceb76398ff122b4d7ea4f61fa61ea
sungwoo93 <thstjddn93@gmail.com>
2019-01-31 16:00:09 +0900

    javascript after page load

--- cam/app.js
+++ cam/app.js
@@ -1,13 +1,14 @@
 var video = document.querySelector("#videoElement");
- 
-if (navigator.mediaDevices.getUserMedia) {       
-    navigator.mediaDevices.getUserMedia({video: true})
-  .then(function(stream) {
-    video.srcObject = stream;
-  })
-  .catch(function(err) {
-    console.log("Something went wrong!");
-  });
-}
 
+window.onload = function () {
+    if (navigator.mediaDevices.getUserMedia) {       
+        navigator.mediaDevices.getUserMedia({video: true})
+    .then(function(stream) {
+        video.srcObject = stream;
+    })
+    .catch(function(err) {
+        console.log("Something went wrong!");
+    });
+    }
+}
 
