04a49f25537a8648029f69c759eed53560949550
sungwoo93 <thstjddn93@gmail.com>
2019-01-31 12:12:09 +0900

    cam webview 2

--- cam/app.js
+++ cam/app.js
@@ -5,7 +5,38 @@ if (navigator.mediaDevices.getUserMedia) {
   .then(function(stream) {
     video.srcObject = stream;
   })
-  .catch(function(err0r) {
+  .catch(function(err) {
     console.log("Something went wrong!");
   });
 }
+
+public class WebCamBridgInterface {
+    public void takePicture() {
+        captureImage();
+    }
+
+    public void showPictures() {
+        Intent intent = new Intent(LandingActivity.this, GalleryActivity.class);
+        startActivityForResult(intent, Constants.REQ_GALLERY);
+    }
+}
+
+function takePicture() {
+    if(typeof AndroidDevice !== "undefined"){
+      AndroidDevice.takePicture();
+    }
+  }
+
+  function showPictures() {
+    if(typeof AndroidDevice !== "undefined"){
+      AndroidDevice.showPictures();
+    }
+  }
+
+  function imageData(data){
+    document.getElementById('displayImage').setAttribute( 'src', 'data:image/png;base64,'+data );
+    if(typeof AndroidDevice !== "undefined"){
+    }
+  }
+  
+webView.addJavascriptInterface(new WebVCamBridgeInterface (), "AndroidDevice");
